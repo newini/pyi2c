1712dea6b92b68907c01c17e66a34848b0b51473
Eunchong Kim <chariskimec@gmail.com>
2021-08-13 11:50:02 +0900

    Added test for i2c

---
+++ tests/i2c.py
@@ -0,0 +1,38 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# tests/i2c.py
+"""
+Test for I2C
+"""
+
+import time
+from pyi2c import I2C
+
+i2c = I2C(0)
+
+AHT10_ADDR = 0x38
+
+# Init
+i2c.write(AHT10_ADDR, 0xe1)
+time.sleep(1)
+
+# loop
+while True:
+    # Trigger
+    i2c.write(AHT10_ADDR, 0xac)
+    time.sleep(.1)
+
+    # Get 6 bytes of data
+    read_data = i2c.read(AHT10_ADDR, 6)
+
+    # Fill data
+    humidity_data = (read_data[1] << 12) + (read_data[2] << 4) + (read_data[3] & 0xf0)
+    temperature_data = ((read_data[3] & 0x0f) << 16) + (read_data[4] << 8) + read_data[5]
+
+    # Convert
+    humidity = humidity_data/(2**20)*100 # in %
+    temperature = temperature_data/(2**20)*200 - 50 # in C
+
+    print('{0:.2f} %, {1:.2f} C'.format(humidity, temperature))
+
+    time.sleep(.9)
