f96f181637eb43c0d75e5cbee6dab1253e7bab66
Eunchong Kim <chariskimec@gmail.com>
2021-08-14 10:53:08 +0900

    Moved directory

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    #package_dir={"": "src"},
+    #packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     install_requires=["smbus2"],
 )
